c15ba5447966cd3933e936f919fc54d8ba6c39ef
Aleix Llusà Serra <10347907+allusa@users.noreply.github.com>
2012-09-13 12:25:08 +0000

    fet un experiment amb dades

--- src/roundrobinson/trunk/roundrobinson.py
+++ src/roundrobinson/trunk/roundrobinson.py
@@ -25,14 +25,14 @@ from discbuffer import Buffer, Disc, ResolutionDisc
 from interpoladors import mitjana, area
                 
             
-class RRD(set):
+class MRD(set):
     """
     Base de dades multiresolució M = {R0,...,Rd} com un conjunt de
     discs resolució
 
     És una subclasse de set.
 
-    >>> M = RRD()
+    >>> M = MRD()
     >>> M.afegeix_disc(5,2,mitjana)
     >>> M.afegeix_disc(10,4,mitjana)
     >>>
@@ -55,7 +55,7 @@ class RRD(set):
     >>> M.consolidable()
     False
     >>> M
-    RRD([RD:Buffer(SerieTemporal([m(10,1), m(10,2), m(40,5)]),0,10),Disc(SerieTemporal([]), |4|), RD:Buffer(SerieTemporal([]),5,5),Disc(SerieTemporal([m(20.0,5)]), |2|)])
+    MRD([RD:Buffer(SerieTemporal([m(10,1), m(10,2), m(40,5)]),0,10),Disc(SerieTemporal([]), |4|), RD:Buffer(SerieTemporal([]),5,5),Disc(SerieTemporal([m(20.0,5)]), |2|)])
     >>> M.update(m4)
     >>> M.consolidable()
     True
@@ -63,7 +63,7 @@ class RRD(set):
     >>> M.consolidable()
     False
     >>> M
-    RRD([RD:Buffer(SerieTemporal([]),10,10),Disc(SerieTemporal([m(27.5,10)]), |4|), RD:Buffer(SerieTemporal([]),10,5),Disc(SerieTemporal([m(20.0,5), m(50.0,10)]), |2|)])
+    MRD([RD:Buffer(SerieTemporal([]),10,10),Disc(SerieTemporal([m(27.5,10)]), |4|), RD:Buffer(SerieTemporal([]),10,5),Disc(SerieTemporal([m(20.0,5), m(50.0,10)]), |2|)])
     >>> M.update(m5)
     >>> M.consolidable()
     True
@@ -71,7 +71,7 @@ class RRD(set):
     >>> M.consolidable()
     False
     >>> M
-    RRD([RD:Buffer(SerieTemporal([m(10,15)]),10,10),Disc(SerieTemporal([m(27.5,10)]), |4|), RD:Buffer(SerieTemporal([]),15,5),Disc(SerieTemporal([m(50.0,10), m(10.0,15)]), |2|)])
+    MRD([RD:Buffer(SerieTemporal([m(10,15)]),10,10),Disc(SerieTemporal([m(27.5,10)]), |4|), RD:Buffer(SerieTemporal([]),15,5),Disc(SerieTemporal([m(50.0,10), m(10.0,15)]), |2|)])
     """
 
     def __init__(self):
