71fa5c2bc8661270c755fc973749c74a0d1015b2
Aleix Llusà Serra <10347907+allusa@users.noreply.github.com>
2013-12-12 13:56:46 +0000

    esquema del pytsms completat

--- src/pytsms/trunk/representation.py
+++ src/pytsms/trunk/representation.py
@@ -14,27 +14,47 @@ Implementació de la representació i graf de Sèrie Temporal.
 from matplotlib import pyplot
 
 from measure import Measure
-from structure import TimeSeriesStructure
-from opseq import TimeSeriesSeqOp
 
 
-class TimeSeriesRepresentation(TimeSeriesStructure):
+
+class Representation(object):
     """
-    Representació com a funció de Sèrie Temporal. Objecte abstracte
-    amb el mètode :py:meth:`TimeSeriesRepresentation.representation`
-    delegat.
+    Representació d'una sèrie temporal. Objecte abstracte amb els
+    mètodes :py:meth:`Representation.interval_temporal` i
+    :py:meth:`Representation.representation` delegats.
+
+    :param s: Sèrie temporal associada a la representació
+    :type s: :py:class:`timeseries.TimeSeries`
+
     """
-
-
-    def __call__(self,t):
+    
+    def __init__(self,s):
         """
-        Crida en funcions
-
-        * s(t)
+        Constructor de la representació amb una sèrie temporal associada
         """
-        return self.representation(t)
+        self._ts = s
+    
+    def get_ts(self):
+        """
+        Retorna la sèrie temporal associada
+        """
+        return self._ts
 
+    def interval_temporal(self,l,g):
+        """
+        Operador d'interval temporal. Sèrie temporal resultant de
+        l'interval temporal sobre la seqüència entre dos temps `l` i `g`.
+        El càlcul de l'interval temporal el realitza la representació `rpr`.
 
+        :param l: Instant de temps menor
+        :type l: :data:`timeseries.Time`
+        :param g:  Instant de temps major
+        :type g: :data:`timeseries.Time`
+        :returns: `s[l,g]ᵗ`
+        :rtype: :class:`timeseries.TimeSeries`
+        :raises NotImplementedError: Abstract method, should be delegated
+        """
+        raise NotImplementedError("Abstract object, method should be delegated")
 
     def representation(self,t):
         """
@@ -48,24 +68,6 @@ class TimeSeriesRepresentation(TimeSeriesStructure):
         """
         raise NotImplementedError("Abstract object, method should be delegated")
 
-
-    def graf(self,l,g,step):
-        """
-        Operador de graf. Llista de parelles (temps,valor) resultants d'evaluar la sèrie temporal en els instant de temps l+n*step < g, n=0,1,2...
-
-        :param l: Instant de temps menor
-        :type l: :data:`timeseries.Time`
-        :param g: Instant de temps major
-        :type g: :data:`timeseries.Time`
-        :param step: Duració de temps
-        :type step: :data:`timeseries.Time`
-        :returns: `graf s(t)ᵗ`
-        :rtype: list of tuples
-        """
-        r = []
-        for t in range(l,g,step):
-            r.append((t,self.representation(t)))
-        return r
 
     def plot(self,l,g,step):
         """
@@ -82,7 +84,7 @@ class TimeSeriesRepresentation(TimeSeriesStructure):
         :type step: :data:`timeseries.Time`
         :returns: pyplot
         """
-        graf = self.graf(l,g,step)
+        graf = self.get_ts().graf(l,g,step)
         x,y = zip(*graf)
 
         pyplot.plot(x,y,'o-')
@@ -93,20 +95,68 @@ class TimeSeriesRepresentation(TimeSeriesStructure):
 
 
 
+"""
+Some representations
+--------------------
+"""
 
 
-class TimeSeriesZohe(TimeSeriesSeqOp,TimeSeriesRepresentation):
+
+
+class DiscretePure(Representation):
+    """
+    Representació discreta pura per a les sèries temporals
+    """
+    pass
+
+
+
+class Zohe(Representation):
     """
     Representació ZOHE de Sèrie Temporal.
 
-    >>> s = TimeSeriesZohe([Measure(5,3),Measure(1,1),Measure(2,2)])
+    >>> from timeseries import TimeSeries
+    >>> s = TimeSeries([Measure(5,3),Measure(1,1),Measure(2,2)])
+    >>> s.set_representation(Zohe)
     >>> s(2) == s.representation(2)
     True
     >>> s(4) == s.representation(4)
     True
     >>> s.graf(1,4,1)
     [(1, 1), (2, 2), (3, 3)]
+    >>> s(2) == s.interval_temporal(2,2).pop().v
+    True
     """
+
+    def interval_temporal(self,l,g):
+        """
+        Operador d'interval temporal ZOHE. 
+
+        :param l: Instant de temps menor
+        :type l: :data:`timeseries.Time`
+        :param g: Instant de temps major
+        :type g: :data:`timeseries.Time`
+        :returns: `s[l,g]ᵗZOHE`
+        :rtype: :class:`timeseries.TimeSeries`
+
+        >>> from timeseries import TimeSeries
+        >>> s = TimeSeries([Measure(5,3),Measure(1,1),Measure(2,2)])
+        >>> s.set_representation(Zohe)
+        >>> s.interval_temporal(1,4) == TimeSeries([Measure(4,3),Measure(2,2)])
+        True
+        >>> s.interval_temporal(-5,0) == TimeSeries([Measure(0,1)])
+        True
+        >>> s.interval_temporal(6,10) == TimeSeries([Measure(10,float("inf"))])
+        True
+        """       
+        s = self.get_ts()
+        r = s.interval_open_left(l,g)
+        v = s[g::'c'].inf().v
+        m = Measure(g,v)
+        r.add(m)
+        return r
+
+
     def representation(self,t):
         """
         Operador de representació ZOHE. Valor resultant d'evaluar la sèrie temporal en l'instant de temps `t` amb representació ZOHE.
@@ -116,7 +166,9 @@ class TimeSeriesZohe(TimeSeriesSeqOp,TimeSeriesRepresentation):
         :returns: `s(t)ZOHE`
         :rtype: :data:`timeseries.Value`
 
-        >>> s = TimeSeriesZohe([Measure(5,3),Measure(1,1),Measure(2,2)])
+        >>> from timeseries import TimeSeries
+        >>> s = TimeSeries([Measure(5,3),Measure(1,1),Measure(2,2)])
+        >>> s.set_representation(Zohe)
         >>> s.representation(2)
         2
         >>> s.representation(4)
@@ -128,12 +180,13 @@ class TimeSeriesZohe(TimeSeriesSeqOp,TimeSeriesRepresentation):
         >>> s.representation(-10)
         1
         """
-        if len(self) == 0:
+        s = self.get_ts()
+        if len(s) == 0:
             return 0
-        if t > self.sup().t:
+        if t > s.sup().t:
             return 0
         
-        return self[t::'c'].inf().v
+        return s[t::'c'].inf().v
 
 
     def plot(self,l=None,g=None,step=None):
@@ -148,17 +201,21 @@ class TimeSeriesZohe(TimeSeriesSeqOp,TimeSeriesRepresentation):
         :type step: :data:`timeseries.Time`
         :returns: pyplot
 
-        >>> s = TimeSeriesZohe([Measure(5,3),Measure(1,1),Measure(2,2)])
-        >>> #s.plot()
+        >>> from timeseries import TimeSeries
+        >>> s = TimeSeries([Measure(5,3),Measure(1,1),Measure(2,2)])
+        >>> s.set_representation(Zohe)
+        >>> #s.get_representation().plot()
         """
+        s = self.get_ts()
         x = [] #x = [float("-inf")]
-        y = [] #y = [self.inf().v]
-        for m in self:
+        y = [] #y = [s.inf().v]
+        for m in sorted(s):
             x.append(m.t)
             y.append(m.v)
 
         #x.append(float("inf"))
         #y.append(0)
 
+        
         pyplot.plot(x,y,'o-',drawstyle='steps-pre')
         pyplot.show()
