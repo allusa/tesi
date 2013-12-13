5ec4520d421365961aabccca985c6d1bc7e227da
Aleix Llusà Serra <10347907+allusa@users.noreply.github.com>
2013-12-13 12:51:39 +0000

    iniciant operadors relacionals

--- src/pytsms/trunk/opset.py
+++ src/pytsms/trunk/opset.py
@@ -21,26 +21,67 @@ from structure import TimeSeriesStructure
 # http://en.wikipedia.org/wiki/Mathematical_operators_and_symbols_in_Unicode
   
 
-
+#'isdisjoint', 'issubset', 'issuperset'
 
 class TimeSeriesSetOpNoTemporal(TimeSeriesStructure):
     """
     Operadors de conjunts no temporals de Sèrie Temporal
     """
 
-    def __or__(self, other):
+    def membership(self,m):
         """
-        Binary arithmetic operation union for sets, symbol `|`.
+        Operador de pertinença. Cert quan la mesura `m` pertany a la sèrie
+        temporal.
+
+        :param m: 
+        :type m: :class:`Measure`
+        :returns: `m ∈ s`, m pertany a s
+        :rtype: bool
+
+        >>> s = TimeSeriesSetOp([Measure(1,2)])
+        >>> m1 = Measure(1,2)
+        >>> m2 = Measure(1,1)
+        >>> s.membership(m1)
+        True
+        >>> s.membership(m2)
+        False
         """
-        return self.union(other)
+        for ms in self:
+            if m.eqnt(ms):
+                return True
+        return False
 
+    def subset(self,s):
+        """
+        Operador de subconjunt. Cert quan la sèrie temporal està
+        inclosa a la sèrie temporal `s`.
 
+        :param s: 
+        :type s: :class:`TimeSeries`
+        :returns: `s1 ⊆ s2`, s1 inclosa a s2
+        :rtype: bool
 
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,1)])
+        >>> s3 = TimeSeriesSetOp([Measure(1,5)])
+        >>> s1.subset(s2)
+        True
+        >>> s2.subset(s1)
+        False
+        >>> s3.subset(s1)
+        False
+        >>> s1.subset(s3)
+        False
+        """
+        for m1 in self:
+            if not s.membership(m1):
+                return False
+        return True
 
     def union(self, other):
         """
         Operador d'unió. Sèrie temporal resultant d'unir la sèrie
-        tempora amb `other`.
+        temporal amb `other`.
 
         :param other: 
         :type other: :class:`TimeSeries`
@@ -56,23 +97,143 @@ class TimeSeriesSetOpNoTemporal(TimeSeriesStructure):
         """
         s = self.copy()
         for m2 in other:
-            if not membership_temporal(m2,s):
+            if not s.membership_temporal(m2):
                 s.add(m2)
         return s
 
+    def difference(self, other):
+        """
+        Operador de diferència Sèrie temporal resultant de treure
+        `other de la sèrie temporal.
+
+        :param other: 
+        :type other: :class:`TimeSeries`
+        :returns: `s1 - s2`, s1 diferència s2
+        :rtype: :class:`TimeSeries`
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
+        >>> s1.difference(s1) == TimeSeriesSetOp([])
+        True
+        >>> s1.difference(s2) == TimeSeriesSetOp([Measure(2,1)])
+        True
+        >>> s2.difference(s1) == TimeSeriesSetOp([Measure(2,2),Measure(3,2)])
+        True
+        """
+        s = type(self)()
+        for m1 in self:
+            if not other.membership(m1):
+                s.add(m1)
+        return s
+
+    def intersection(self, other):
+        """
+        Operador d'intersecció. Sèrie temporal resultant
+        d'interseccionar `other amb la sèrie temporal.
+
+        :param other: 
+        :type other: :class:`TimeSeries`
+        :returns: `s1 ∩ s2`, s1 intersecció s2
+        :rtype: :class:`TimeSeries`
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
+        >>> s1.intersection(s1) == s1
+        True
+        >>> s1.intersection(s2) == TimeSeriesSetOp([Measure(1,2)])
+        True
+        >>> s2.intersection(s1) == s1.intersection(s2)
+        True
+        """
+        return self.difference(self.difference(other))
+
+
+    def symmetric_difference(self, other):
+        """
+        Operador de diferència simètrica. Sèrie temporal resultant
+        de la diferènca simètrica d'`other amb la sèrie temporal.
+
+        :param other: 
+        :type other: :class:`TimeSeries`
+        :returns: `s1 ⊖ s2`, s1 diferència simètrica s2
+        :rtype: :class:`TimeSeries`
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
+        >>> s1.symmetric_difference(s1) == TimeSeriesSetOp([])
+        True
+        >>> s1.symmetric_difference(s2) == TimeSeriesSetOp([Measure(2,1),Measure(3,2)])
+        True
+        >>> s2.symmetric_difference(s1) == TimeSeriesSetOp([Measure(2,2),Measure(3,2)])
+        True
+        """
+        return self.difference(other).union(other.difference(self))
 
 
 
-class TimeSeriesSetOpTemporal(object):
+
+class TimeSeriesSetOpTemporal(TimeSeriesStructure):
     """
     Operadors de conjunts temporals de Sèrie Temporal
     """
 
-    def __or__(self, other):
+
+    def membership_temporal(self,m):
         """
-        Binary arithmetic operation union for sets, symbol `|`.
+        Operador de pertinença temporal. Cert quan la mesura `m` pertany
+        temporalment a la sèrie temporal.
+
+        Cerca si m.t pertany al conjunt de temps de s (s.t())
+        Sempre es compleix que si membership(m,S)==True aleshores
+        membership_temporal(m,S)==True
+
+        :param m: 
+        :type m: :class:`Measure`
+        :returns: `m ∈ᵗ s`, m pertany temporalment a s
+        :rtype: bool
+
+        >>> s = TimeSeriesSetOp([Measure(1,2)])
+        >>> m1 = Measure(1,2)
+        >>> m2 = Measure(1,1)
+        >>> s.membership_temporal(m1)
+        True
+        >>> s.membership_temporal(m2)
+        True
+        >>> s.membership_temporal(Measure(2,2))
+        False
         """
-        return self.union_temporal(other)
+        return m.t in self.t()
+
+
+    def subset_temporal(self,s):
+        """
+        Operador de subconjunt temporal. Cert quan la sèrie temporal està
+        inclosa temporalment a la sèrie temporal `s`.
+
+        Sempre es compleix que si s1.subset(s2)==True aleshores
+        s1.subset_temporal(s2)==True
+
+        :param s: 
+        :type s: :class:`TimeSeries`
+        :returns: `s1 ⊆ᵗ s2`, s1 inclosa temporalment a s2
+        :rtype: bool
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,1)])
+        >>> s3 = TimeSeriesSetOp([Measure(1,5)])
+        >>> s1.subset_temporal(s2)
+        True
+        >>> s2.subset_temporal(s1)
+        False
+        >>> s1.subset_temporal(s3)
+        False
+        >>> s3.subset_temporal(s1)
+        True
+        """
+        for m1 in self:
+            if not s.membership_temporal(m1):
+                return False
+        return True
 
 
     def union_temporal(self, other):
@@ -94,189 +255,168 @@ class TimeSeriesSetOpTemporal(object):
         """
         s = type(self)()
         for m1 in self:
-            if not membership_temporal(m1,other):
+            if not other.membership_temporal(m1):
                 s.add(m1)
-            elif membership(m1,other):
+            elif other.membership(m1):
                 s.add(m1)
 
         for m2 in other:
-            if not membership_temporal(m2,self):
+            if not self.membership_temporal(m2):
                 s.add(m2)
         return s
 
+    def difference_temporal(self, other):
+        """
+        Operador de diferència temporal. Sèrie temporal resultant de
+        treure temporalment `other de la sèrie temporal.
+
+        :param other: 
+        :type other: :class:`TimeSeries`
+        :returns: `s1 -ᵗ s2`, s1 diferència temporal s2
+        :rtype: :class:`TimeSeries`
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
+        >>> s1.difference_temporal(s1) == s1.difference(s1)
+        True
+        >>> s1.difference_temporal(s2) == TimeSeriesSetOp([])
+        True
+        >>> s2.difference_temporal(s1) == TimeSeriesSetOp([Measure(3,2)])
+        True
+        """
+        s = type(self)()
+        for m1 in self:
+            if not other.membership_temporal(m1):
+                s.add(m1)
+        return s
+
+    def intersection_temporal(self, other):
+        """
+        Operador d'intersecció temporal. Sèrie temporal resultant
+        d'interseccionar temporalment `other amb la sèrie temporal.
+
+        :param other: 
+        :type other: :class:`TimeSeries`
+        :returns: `s1 ∩ᵗ s2`, s1 intersecció temporal s2
+        :rtype: :class:`TimeSeries`
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
+        >>> s1.intersection_temporal(s1) == s1.intersection(s1)
+        True
+        >>> s1.intersection_temporal(s2) == TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        True
+        >>> s2.intersection_temporal(s1) == TimeSeriesSetOp([Measure(1,2),Measure(2,2)])
+        True
+        """
+        return self.difference_temporal(self.difference_temporal(other))
+
+
+    def symmetric_difference_temporal(self, other):
+        """
+        Operador de diferència simètrica temporal. Sèrie temporal resultant
+        de la diferènca simètrica temporal d'`other amb la sèrie temporal.
+
+        :param other: 
+        :type other: :class:`TimeSeries`
+        :returns: `s1 ⊖ᵗ s2`, s1 diferència simètrica temporal s2
+        :rtype: :class:`TimeSeries`
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+        >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
+        >>> s1.symmetric_difference_temporal(s1) == s1.symmetric_difference(s1)
+        True
+        >>> s1.symmetric_difference_temporal(s2) == TimeSeriesSetOp([Measure(3,2)])
+        True
+        >>> s2.symmetric_difference_temporal(s1) == s1.symmetric_difference_temporal(s2)
+        True
+        """
+        return self.difference_temporal(other).union_temporal(other.difference_temporal(self))
 
 
 
 
-class TimeSeriesSetOp(TimeSeriesSetOpNoTemporal,TimeSeriesSetOpTemporal):
+class TimeSeriesSetOpRelacional(TimeSeriesStructure):
+    """
+    Operadors de conjunts relacionals de Sèrie Temporal
+    """
+
+    def projection(self,A):
+        """
+        Operador de projecció. Conjunt resultant de seleccionar
+        els atributs (columnes) `A` de la sèrie temporal.
+
+        :param A: exemple ['t','v']
+        :type A: Iterable of attribute names
+        :returns: `s{A}`, projecció de s en A
+        :rtype: set
+
+        >>> s1 = TimeSeriesSetOp([Measure(1,1),Measure(2,1)])
+        >>> TimeSeriesSetOp([]).projection([]) == set()
+        True
+        >>> s1.projection([])  == set([])
+        True
+        >>> s1.projection(['t']) == set([1,2])
+        True
+        >>> s1.projection(['v']) == set([1])
+        True
+        >>> s1.projection(['t','v']) == set([(1,1),(2,1)])
+        True
+        """
+        s = set()       
+        for m in self:
+            row = []
+            for a in A:              
+                if a == 't':
+                    row.append(m.t)
+                if a == 'v':
+                    row.append(m.v)
+
+            if len(row) > 1:
+                s.add(tuple(row))
+            elif len(row) == 1:
+                s.add(row[0])
+            else:
+                pass
+        return s
+
+
+
+
+
+
+
+
+class TimeSeriesSetOp( TimeSeriesSetOpRelacional,TimeSeriesSetOpNoTemporal,TimeSeriesSetOpTemporal):
     """
     Operadors de conjunts de Sèrie Temporal, inclou els Temporals i
     els no Temporals.
-    """
-    pass
 
-
-
-
-
-
-
-
-
-class TimeSeriesNoTemporal(TimeSeriesSetOpNoTemporal,TimeSeriesSetOpTemporal):
-    """
-    >>> s1 = TimeSeriesNoTemporal([Measure(1,2),Measure(2,1)])
-    >>> s2 = TimeSeriesNoTemporal([Measure(3,2),Measure(1,2),Measure(2,2)])
-    >>> s2 | s1 == s2
-    True
-    >>> s1 | s2 == TimeSeriesNoTemporal([Measure(1,2), Measure(3,2), Measure(2,1)])
-    True
+    >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
+    >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,2)])
     >>> s1 | s2 == s1.union(s2)
     True
-    >>> s1.temporal() | s2 == TimeSeriesNoTemporal([Measure(1,2), Measure(3,2)])
-    True
-    >>> s1.temporal() | s2 == s1.union_temporal(s2)
+    >>> s2 | s1 == s2.union(s1)
     True
     """
-    def temporal(self):
+    def __or__(self, other):
         """
-        Per a cridar els operadors de conjunts temporals
+        Binary arithmetic operation union for sets, symbol `|`.
         """
-        s = self.copy()
-        s.__class__ = TimeSeriesTemporal
-        return s
-
-
-class TimeSeriesTemporal(TimeSeriesSetOpTemporal,TimeSeriesSetOpNoTemporal):
-
-
-    def _notemporal(self):
-        """
-        Per a no cridar els operadors de conjunts temporals
-        """
-        s = self.copy()
-        s.__class__ = TimeSeriesNoTemporal
-        return s
+        return self.union(other)
 
 
 
 
 
-def membership(m,s):
-    """
-    Operador de pertinença. Cert quan la mesura `m` pertany a la sèrie
-    temporal `s`.
-
-    :param m: 
-    :type m: :class:`Measure`
-    :param s: 
-    :type s: :class:`TimeSeries`
-    :returns: `m ∈ s`, m pertany a s
-    :rtype: bool
-
-    >>> s = TimeSeriesSetOp([Measure(1,2)])
-    >>> m1 = Measure(1,2)
-    >>> m2 = Measure(1,1)
-    >>> membership(m1,s)
-    True
-    >>> membership(m2,s)
-    False
-    """
-    for ms in s:
-        if m.eqnt(ms):
-            return True
-    return False
-        
-
-def membership_temporal(m,s):
-    """
-    Operador de pertinença temporal. Cert quan la mesura `m` pertany
-    temporalment a la sèrie temporal `s`.
-
-    Cerca si m.t pertany al conjunt de temps de s (s.t())
-    Sempre es compleix que si membership(m,S)==True aleshores
-    membership_temporal(m,S)==True
-
-    :param m: 
-    :type m: :class:`Measure`
-    :param s: 
-    :type s: :class:`TimeSeries`
-    :returns: `m ∈ᵗ s`, m pertany temporalment a s
-    :rtype: bool
-
-    >>> s = TimeSeriesSetOp([Measure(1,2)])
-    >>> m1 = Measure(1,2)
-    >>> m2 = Measure(1,1)
-    >>> membership_temporal(m1,s)
-    True
-    >>> membership_temporal(m2,s)
-    True
-    >>> membership_temporal(Measure(2,2),s)
-    False
-    """
-    return m.t in s.t()
 
 
-def subset(s1,s2):
-    """
-    Operador de subconjunt. Cert quan la sèrie temporal `s1` està
-    inclosa a la sèrie temporal `s2`.
 
-    :param s1: 
-    :type s1: :class:`TimeSeries`
-    :param s2: 
-    :type s2: :class:`TimeSeries`
-    :returns: `s1 ⊆ s2`, s1 inclosa a s2
-    :rtype: bool
 
-    >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
-    >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,1)])
-    >>> s3 = TimeSeriesSetOp([Measure(1,5)])
-    >>> subset(s1,s2)
-    True
-    >>> subset(s2,s1)
-    False
-    >>> subset(s3,s1)
-    False
-    >>> subset(s1,s3)
-    False
-    """
-    for m1 in s1:
-        if not membership(m1,s2):
-            return False
-    return True
 
-def subset_temporal(s1,s2):
-    """
-    Operador de subconjunt temporal. Cert quan la sèrie temporal `s1` està
-    inclosa temporalment a la sèrie temporal `s2`.
-    
-    Sempre es compleix que si subset(s1,s2)==True aleshores
-    subset_temporal(s1,s2)==True
 
-    :param s1: 
-    :type s1: :class:`TimeSeries`
-    :param s2: 
-    :type s2: :class:`TimeSeries`
-    :returns: `s1 ⊆ᵗ s2`, s1 inclosa temporalment a s2
-    :rtype: bool
 
-    >>> s1 = TimeSeriesSetOp([Measure(1,2),Measure(2,1)])
-    >>> s2 = TimeSeriesSetOp([Measure(3,2),Measure(1,2),Measure(2,1)])
-    >>> s3 = TimeSeriesSetOp([Measure(1,5)])
-    >>> subset_temporal(s1,s2)
-    True
-    >>> subset_temporal(s2,s1)
-    False
-    >>> subset_temporal(s1,s3)
-    False
-    >>> subset_temporal(s3,s1)
-    True
-    """
-    for m1 in s1:
-        if not membership_temporal(m1,s2):
-            return False
-    return True
+
 
 
 
