36eeed889af7b79d196ca8b81768d56ee20c1629
Aleix Llusà Serra <10347907+allusa@users.noreply.github.com>
2014-03-28 13:23:26 +0000

    impl en rel

--- doc/implementacions/trunk/mtsms.d
+++ doc/implementacions/trunk/mtsms.d
@@ -1,6 +1,23 @@
 
 
 
+VAR mtsms.multiresolution BASE RELATION
+    {sb same_type_as(timeseries), sd same_type_as(timeseries), tau RATIONAL, delta RATIONAL, f CHARACTER, k RATIONAL} KEY { delta, f } ;
+
+
+
+
+
+
+OPERATOR mtsms.add(ms SAME_TYPE_AS  (mtsms.multiresolution),m SAME_TYPE_AS  (timeseries)) RETURNS SAME_TYPE_AS (mtsms.multiresolution);
+//   return ts.union(S FROM TUPLE FROM b,m);
+END OPERATOR;
+
+
+
+
+
+
 VAR mtsms.buffers BASE RELATION
     {S same_type_as(timeseries), tau RATIONAL, delta RATIONAL, f CHARACTER} KEY { delta, f } ;
 
